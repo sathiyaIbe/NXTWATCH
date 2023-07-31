742bf5d87a40e974f586f9be00b7fb7db9f2b088
unknown <sathiyaibe@gmail.com>
2023-07-31 11:30:29 +0530

    created

---
+++ src/components/Home/index.js
@@ -0,0 +1,188 @@
+import {Component} from 'react'
+import Cookies from 'js-cookie'
+import Loader from 'react-loader-spinner'
+
+import {AiOutlineClose, AiOutlineSearch} from 'react-icons/ai'
+
+import Header from '../Header'
+import NavigationBar from '../NavigationBar'
+import ThemeAndVideoContext from '../../context/ThemeAndVideoContext'
+import HomeVideos from '../HomeVideos'
+import FailureView from '../FailureView'
+
+import {
+  HomeContainer,
+  BannerContainer,
+  BannerImage,
+  BannerText,
+  BannerButton,
+  BannerLeftPart,
+  BannerRightPart,
+  BannerCloseButton,
+  SearchContainer,
+  SearchInput,
+  SearchIconContainer,
+  LoaderContainer,
+} from './styledComponents'
+
+const apiStatusConstants = {
+  initial: 'INITIAL',
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  inProgress: 'IN_PROGRESS',
+}
+
+class Home extends Component {
+  state = {
+    homeVideos: [],
+    searchInput: '',
+    apiStatus: apiStatusConstants.initial,
+  }
+
+  componentDidMount() {
+    this.getVideos()
+  }
+
+  getVideos = async () => {
+    const {searchInput} = this.state
+    this.setState({apiStatus: apiStatusConstants.inProgress})
+    const jwtToken = Cookies.get('jwt_token')
+    const url = `https://apis.ccbp.in/videos/all?search=${searchInput}`
+    const options = {
+      headers: {
+        Authorization: `Bearer ${jwtToken}`,
+      },
+      method: 'GET',
+    }
+    const response = await fetch(url, options)
+    if (response.ok) {
+      const data = await response.json()
+
+      const updatedData = data.videos.map(eachVideo => ({
+        id: eachVideo.id,
+        title: eachVideo.title,
+        thumbnailUrl: eachVideo.thumbnail_url,
+        viewCount: eachVideo.view_count,
+        publishedAt: eachVideo.published_at,
+        name: eachVideo.channel.name,
+        profileImageUrl: eachVideo.channel.profile_image_url,
+      }))
+      this.setState({
+        homeVideos: updatedData,
+        apiStatus: apiStatusConstants.success,
+      })
+    } else {
+      this.setState({apiStatus: apiStatusConstants.failure})
+    }
+  }
+
+  onChangeInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  getSearchResults = () => {
+    this.getVideos()
+  }
+
+  onRetry = () => {
+    this.setState({searchInput: ''}, this.getVideos)
+  }
+
+  renderLoadingView = () => (
+    <LoaderContainer data-testid="loader">
+      <Loader type="TailSpin" color="#ff0000" height="50" width="50" />
+    </LoaderContainer>
+  )
+
+  renderVideosView = () => {
+    const {homeVideos} = this.state
+    return <HomeVideos homeVideos={homeVideos} onRetry={this.onRetry} />
+  }
+
+  renderFailureView = () => <FailureView onRetry={this.onRetry} />
+
+  renderHomeVideos = () => {
+    const {apiStatus} = this.state
+
+    switch (apiStatus) {
+      case apiStatusConstants.success:
+        return this.renderVideosView()
+      case apiStatusConstants.failure:
+        return this.renderFailureView()
+      case apiStatusConstants.inProgress:
+        return this.renderLoadingView()
+      default:
+        return null
+    }
+  }
+
+  render() {
+    const {searchInput} = this.state
+
+    return (
+      <ThemeAndVideoContext.Consumer>
+        {value => {
+          const {isDarkTheme, showHomeCard, updateHomeHomeBannerView} = value
+
+          const onCloseBanner = () => {
+            updateHomeHomeBannerView()
+          }
+
+          const bgColor = isDarkTheme ? '#181818' : '#f9f9f9'
+          const textColor = isDarkTheme ? '#f9f9f9' : '#231f20'
+
+          return (
+            <>
+              <Header />
+              <NavigationBar />
+              <HomeContainer data-testid="home" bgColor={bgColor}>
+                {showHomeCard && (
+                  <BannerContainer data-testid="banner">
+                    <BannerLeftPart>
+                      <BannerImage
+                        src="https://assets.ccbp.in/frontend/react-js/nxt-watch-logo-light-theme-img.png"
+                        alt="nxt watch logo"
+                      />
+                      <BannerText>
+                        Buy Nxt Watch Premium prepaid plans with UPI
+                      </BannerText>
+                      <BannerButton type="button">GET IT NOW</BannerButton>
+                    </BannerLeftPart>
+                    <BannerRightPart>
+                      <BannerCloseButton
+                        data-testid="close"
+                        onClick={onCloseBanner}
+                        type="button"
+                      >
+                        <AiOutlineClose size={25} />
+                      </BannerCloseButton>
+                    </BannerRightPart>
+                  </BannerContainer>
+                )}
+                <SearchContainer>
+                  <SearchInput
+                    type="search"
+                    placeholder="Search"
+                    value={searchInput}
+                    onChange={this.onChangeInput}
+                    color={textColor}
+                  />
+                  <SearchIconContainer
+                    data-testid="searchButton"
+                    onClick={this.getSearchResults}
+                    type="button"
+                  >
+                    <AiOutlineSearch size={20} />
+                  </SearchIconContainer>
+                </SearchContainer>
+                {this.renderHomeVideos()}
+              </HomeContainer>
+            </>
+          )
+        }}
+      </ThemeAndVideoContext.Consumer>
+    )
+  }
+}
+
+export default Home
---
+++ src/context/ThemeAndVideoContext.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ThemeAndVideoContext = React.createContext({
+  isDarkTheme: false,
+  savedVideos: [],
+  activeTab: 'Home',
+  showHomeCard: true,
+  updateHomeHomeBannerView: () => {},
+  toggleTheme: () => {},
+  changeTab: () => {},
+  addVideo: () => {},
+})
+
+export default ThemeAndVideoContext
